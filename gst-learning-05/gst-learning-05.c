d468a5f9899626bbe7833122bb29dedb99d11413
Wenhao Zhang <wenhao.zhang@hulu.com>
2021-12-28 18:54:56 +0800

    Started but gave up the tutorial 5, because it's a GTK related project.

---
+++ gst-learning-05/gst-learning-05.c
@@ -0,0 +1,11 @@
+//
+//  gst-learning-05.c
+//  gst-learning-05
+//
+//  Created by Wenhao Zhang on 2021/12/28.
+
+#include <string.h>
+
+#include <gtk/gtk.h>
+#include <gst/gst.h>
+#include <gst/video/videooverlay.h>
